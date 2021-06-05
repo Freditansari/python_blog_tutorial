41002e5f889a28e243e07552f77133b304d24962
Freditansari <Freditansari@users.noreply.github.com>
2021-06-05 22:41:37 +0700

    register forms were made including email

--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms import validators
 from wtforms.fields.core import BooleanField
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.fields.html5 import EmailField
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
                         validators=[DataRequired(), 
                         Length(min=2, max=20)])
-    email = StringField('Email', 
-            validators=[DataRequired(), Email()])
+    email = EmailField('Email', 
+            validators=[ Email(), DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
