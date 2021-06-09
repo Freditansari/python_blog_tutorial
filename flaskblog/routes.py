b2d4c226b6ff6a73bc26ef68950f34946902cc2b
freditansari <freditansari@gmail.com>
2021-06-09 10:37:45 +0700

    made the sign in and sign out becomes responsive

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -38,7 +38,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
@@ -55,8 +55,20 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect (url_for('home'))
+            return redirect(url_for('home'))
         else:
             flash('login unsucessful', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
